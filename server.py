46da3419b47e390c098588dc40336eb8de5ffdd3
Shamal Chathuranga Wijesuriya <shamalchathuranga2@gmail.com>
2024-05-20 20:54:17 +0530

    files added

---
+++ server.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from joblib import load
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize FastAPI app
+app = FastAPI()
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Message(BaseModel):
+    message: str
+
+# Load the model and vectorizer once at the start
+vectorizer = load('tfidf_vectorizer.joblib')
+model = load('spam_detection_model.joblib')
+
+@app.post("/api/classify")
+async def classify_message(message: Message):
+    
+
+
+# Run the server
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3005)
