5e8e39c265e75e6903a4dc1b3a19c3886c9b4d53
naveen-dilsara <naveendilsara2@gmail.com>
2024-05-21 08:48:18 +0530

    Model return issue Fixed

--- server.py
+++ server.py
@@ -25,7 +25,21 @@ model = load('spam_detection_model.joblib')
 @app.post("/api/classify")
 async def classify_message(message: Message):
     try:
-        
+        # Vectorize the message
+        X = vectorizer.transform([message.message])
+
+        # Predict using the model
+        prediction = model.predict(X)
+
+        # Convert prediction to a list for JSON serialization
+        prediction_list = prediction.tolist()
+
+        # Return the prediction
+        if prediction_list[0] == 1:
+            return  True
+        else:
+            return False
+            
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
